59868a60ecd3d88361cebc70fd3027c8dde4c6fc
MLTN <militanmawira@gmail.com>
2026-07-15 14:45:34 +0300

    Replace smsg import with mongoSession imports

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const express = require("express");
 const { DateTime } = require("luxon");
 const util = require("util");
 const speed = require("performance-now");
-const { smsg } = require('./lib/smsg');
+const { downloadSession, uploadSession } = require('./lib/mongoSession');
 const fetchLogoUrl = require('./lib/ephoto');
 const {
   smsgsmsg, formatp, tanggal, formatDate, getTime, sleep, clockString,
